6bf2a63a4eab7eab1149ad4c3eb4a73370113f38
WerWolv <werwolv98@gmail.com>
2022-10-11 15:18:42 +0200

    docs: Added new keywords to hexpat highlighting

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -121,9 +121,9 @@ class PatternLanguageLexer(RegexLexer):
 
     tokens = {
         'root': [
-            (r'(?<![a-zA-Z0-9_])(using|struct|union|enum|bitfield|be|le|fn|return|in|out|continue|break)(?![a-zA-Z0-9_])', token.Keyword.Reserved),
-            (r'(?<![a-zA-Z0-9_])(u8|u16|u32|u64|u128|s8|s16|s32|s64|s128|float|double|char|char16|bool)(?![a-zA-Z0-9_])', token.Keyword.Type),
-            (r'(?<![a-zA-Z0-9_])(padding|while|if|else|true|false|parent|addressof|sizeof|namespace|\$)(?![a-zA-Z0-9_])', token.Keyword.Type),
+            (r'(?<![a-zA-Z0-9_])(using|struct|union|enum|bitfield|be|le|fn|return|in|out|continue|break|ref|parent|addressof|sizeof|namespace|padding)(?![a-zA-Z0-9_])', token.Keyword.Reserved),
+            (r'(?<![a-zA-Z0-9_])(u8|u16|u24|u32|u48|u64|u96|u128|s8|s16|s24|s32|s48|s64|s96|s128|float|double|char|char16|bool|auto|str)(?![a-zA-Z0-9_])', token.Keyword.Type),
+            (r'(?<![a-zA-Z0-9_])(while|for|if|else|true|false|\$)(?![a-zA-Z0-9_])', token.Keyword.Type),
             (r'//.+', token.Comment),
             (r'[\{\}\[\]\;\:\=\.\,\@\(\)\>\<\+\-\*\/\!]', token.Operator),
             (r'(#.+)', token.Comment.Preproc),
