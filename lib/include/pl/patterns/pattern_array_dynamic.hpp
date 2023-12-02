b5de2d2f8aa073c8de3a0bc419b43a4a73070ae3
WerWolv <werwolv98@gmail.com>
2023-12-02 14:35:06 +0100

    patterns: Fix reading raw bytes from sealed types

--- lib/include/pl/patterns/pattern_array_dynamic.hpp
+++ lib/include/pl/patterns/pattern_array_dynamic.hpp
@@ -204,10 +204,15 @@ namespace pl::ptrn {
         std::vector<u8> getRawBytes() override {
             std::vector<u8> result;
 
-            this->forEachEntry(0, this->getEntryCount(), [&](u64, pl::ptrn::Pattern *entry) {
-                auto bytes = entry->getBytes();
-                std::copy(bytes.begin(), bytes.end(), std::back_inserter(result));
-            });
+            if (this->isSealed()) {
+                result.resize(this->getSize());
+                this->getEvaluator()->readData(this->getOffset(), result.data(), result.size(), this->getSection());
+            } else {
+                this->forEachEntry(0, this->getEntryCount(), [&](u64, pl::ptrn::Pattern *entry) {
+                    auto bytes = entry->getBytes();
+                    std::copy(bytes.begin(), bytes.end(), std::back_inserter(result));
+                });
+            }
 
             return result;
         }
