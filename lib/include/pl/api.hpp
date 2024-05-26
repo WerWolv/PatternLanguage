094027929f6af9d25c931121e594320d0ac16322
David Mentler <david.mentler@shapr3d.com>
2024-05-26 14:37:17 +0200

    Sections API

--- lib/include/pl/api.hpp
+++ lib/include/pl/api.hpp
@@ -9,6 +9,7 @@
 #include <functional>
 #include <string>
 #include <optional>
+#include <span>
 
 namespace pl {
 
@@ -58,12 +59,90 @@ namespace pl::api {
 
     };
 
+    class Section {
+    public:
+        using IOError = std::optional<std::string>;
+        
+        using ChunkReader = std::function<IOError(std::span<const u8>)>;
+        using ChunkWriter = std::function<IOError(std::span<u8>)>;
+
+        virtual ~Section() {};
+        
+        /// Read data from this section into the provided contiguous buffer
+        IOError read(u64 fromAddress, std::span<u8> into) const;
+
+        /// Read data from this section - the section will call `reader` with the largest contiguous chunks it can provide
+        IOError read(u64 fromAddress, size_t size, ChunkReader& reader) const;
+        
+        /// Write data to this section from the provided contiguous buffer
+        IOError write(bool expand, u64 toAddress, std::span<const u8> from);
+
+        /// Write data to this section - the section will call `writer` with the largest contiguous chunks it can provide
+        IOError write(bool expand, u64 toAddress, size_t size, ChunkWriter& writer);
+
+        /// Write data to this section from the provided section
+        IOError write(bool expand, u64 toAddress, size_t size, u64 fromAddress, api::Section& fromSection);
+
+        /// Access the size of this section
+        virtual size_t size() const = 0;
+        
+        /// Shrink or expand this section to the specified size
+        virtual IOError resize(size_t newSize) = 0;
+
+        struct ChunkAttributes {
+            enum class Type {
+                /// Represents a chunk of unknown quality - returned when the section implementation
+                /// would need to fetch the data to reason about it's attributes, or does not support
+                /// chunk attribute access
+                Unknown,
+                
+                /// Represents a chunk of address space with normal data inside
+                Generic,
+                
+                /// Represents a chunk of address space with no data inside
+                Unmapped,
+                
+                /// Represents a chunk of address space with nothing but zeros - note that section implementations
+                /// are not required to detect such repeating sections
+                Zeros,
+            };
+            
+            /// Type of this chunk
+            Type type = Type::Generic;
+            
+            /// Start address of this chunk
+            u64 baseAddress = 0x00;
+            
+            /// Size of the chunk
+            size_t size = 0;
+            
+            /// Whether the underlying bytes are writable
+            bool writable = false;
+        };
+        using ChunkAttributesReader = std::function<bool(const ChunkAttributes&)>;
+        
+        /// Read all attributed chunks overlapping the specified area - sections are guaranteed to produce non-overlapping contiguous chunks in their
+        /// address space, however the first/last chunks are not guaranteed to fit into the specified window.
+        ///
+        /// The provided `ChunkAttributesReader`  may return `true` to stop iterating chunk attributes.
+        ///
+        /// \returns Whether reading was interrupted by `reader`
+        virtual bool readChunkAttributes(u64 fromAddress, size_t size, ChunkAttributesReader& reader) const = 0;
+        
+    protected:
+        /// Performs a chunked write operation - incoming parameters have been already validated
+        virtual IOError readRaw(u64 fromAddress, size_t size, ChunkReader& reader) const = 0;
+
+        /// Performs a chunked write operation - incoming parameters have been already validated
+        virtual IOError writeRaw(u64 toAddress, size_t size, ChunkWriter& writer) = 0;
+    };
+
     /**
      * @brief A type representing a custom section
      */
-    struct Section {
+    struct CustomSection {
         std::string name;
-        std::vector<u8> data;
+        std::unique_ptr<Section> section;
     };
 
     /**
