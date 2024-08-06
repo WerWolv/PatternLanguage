513721860660f44b4200cc5d00a9fc5282f03d1c
paxcut <paxcut@outlook.com>
2024-08-06 15:33:54 -0700

    Fix: When a string ends prematurely with a newline, the newline wasn't increasing the number of lines so all subsequent tokens had the wrong line location. This was causing heap corruption errors in some cases when it should be causing index out of bounds errors like in the other cases which is kind of suspicious.

--- lib/source/pl/core/lexer.cpp
+++ lib/source/pl/core/lexer.cpp
@@ -176,9 +176,17 @@ namespace pl::core {
 
         while (m_sourceCode[m_cursor] != '\"') {
             char c = peek();
-            if (c == '\n' || c == '\0') {
+            if (c == '\n') {
                 m_errorLength = 1;
-                error(c == '\n' ? "Unexpected newline in string literal" : "Unexpected end of file in string literal");
+                error("Unexpected newline in string literal");
+                m_line++;
+                m_lineBegin = m_cursor;
+                return std::nullopt;
+            }
+
+            if (c == '\0') {
+                m_errorLength = 1;
+                error("Unexpected end of file in string literal");
                 return std::nullopt;
             }
 
