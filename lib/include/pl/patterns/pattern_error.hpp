4295ce65f350ea2688eab3a951bb6b14ff6407d6
shewitt.au <shewitt.au@gmail.com>
2025-06-02 04:26:20 +1000

    WIP

--- lib/include/pl/patterns/pattern_error.hpp
+++ lib/include/pl/patterns/pattern_error.hpp
@@ -5,12 +5,34 @@
 namespace pl::ptrn {
 
     class PatternError : public Pattern {
-    public:
+    protected:
+        void initialise(core::Evaluator *evaluator, u64 offset, size_t size, u32 line, std::string errorMessage) {
+            (void)errorMessage;
+            Pattern::initialise(evaluator, offset, size, line);
+        }
+
+        void initialise(const PatternError &other) {
+            Pattern::initialise(other);
+        }
+
         PatternError(core::Evaluator *evaluator, u64 offset, size_t size, u32 line, std::string errorMessage)
             : Pattern(evaluator, offset, size, line), m_errorMessage(std::move(errorMessage)) { }
 
+    public:
+        static std::shared_ptr<PatternError> create(core::Evaluator *evaluator, u64 offset, size_t size, u32 line, std::string errorMessage) {
+            auto p = std::shared_ptr<PatternError>(new PatternError(evaluator, offset, size, line, errorMessage));
+            p->initialise(evaluator, offset, size, line, errorMessage);
+            return p;
+        }
+
+        static std::shared_ptr<PatternError> create(const PatternError &other) {
+            auto p = std::shared_ptr<PatternError>(new PatternError(other));
+            p->initialise(other);
+            return p;
+        }
+
         [[nodiscard]] std::shared_ptr<Pattern> clone() const override {
-            return std::shared_ptr<Pattern>(new PatternError(*this));
+            return create(*this);
         }
 
         [[nodiscard]] std::string getFormattedName() const override {
