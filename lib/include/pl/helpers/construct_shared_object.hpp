c6095e6803580ddebe9017b8baee062ad97874e9
shewitt.au <shewitt.au@gmail.com>
2025-06-03 09:09:12 +1000

    Change comment

--- lib/include/pl/helpers/construct_shared_object.hpp
+++ lib/include/pl/helpers/construct_shared_object.hpp
@@ -1,8 +1,11 @@
 // construct_shared_object.hpp
 //
+// Written by Stephen Hewitt in 2025
+// GitHub repo: https://github.com/shewitt-au/std-enable_shared_from_this-and-constructors
 
 #pragma once
 /*
+
 The motivation for this code is issues with std::shared_ptr and
 std::enable_shared_from_this.
 
@@ -12,7 +15,7 @@ Consider code like this:
 Assume that `SomeClass`'s constructor uses shared_from_this. In this case, since
 SomeClass is not yet owned by a std::shared_ptr, the shared_from_this call will
 not work as expected. There seems to no way around this that I can find.
-I wasn�t aware of this issue. It was a real kick in the pants.
+I wasn't aware of this issue. It was a real kick in the pants.
 
 The solution implemented here is two-stage construction. First the object is
 constructed and assigned to a std::shared_ptr; then, if present, a
@@ -22,9 +25,9 @@ The intent of the "if present" is to enable incremental migration to two-stage
 construction on demand.
 
 Another issue addressed is with std::make_shared. This function requires that
-the constructor be public. If we�re making factory methods for creation, we may
+the constructor be public. If we're making factory methods for creation, we may
 want to make the constructors non-public to stop misuse. In this case the code
-uses std::shared_ptr�s constructor and a new call. The function that calls new
+uses std::shared_ptr's constructor and a new call. The function that calls new
 can be made a friend of the class to grant it access. We lose the advantages of
 using std::make_shared but gain protection against incorrect object
 instantiation.
@@ -45,7 +48,7 @@ namespace shared_object_creator {
         /*
         shared_ptr_creator is responsible for actually creating the std::shared_ptr.
         As described in this header's opening comment, it decides whether to use
-        std::make_shared or std::shared_ptr�s constructor and new based on the
+        std::make_shared or std::shared_ptr's constructor and new based on the
         accessibility the the class' constructor. If you wish to make the constructors
         non-public make this a friend. The BEFRIEND_SHARED_OBJECT_CREATOR macro can be
         used to do this tersely.
