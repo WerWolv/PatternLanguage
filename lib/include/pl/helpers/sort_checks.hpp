caeb6ea5bd860f235177afdf3d52a4c94b55a88b
shewitt.au <shewitt.au@gmail.com>
2025-07-29 05:41:22 +1000

    Undo lunacy

--- lib/include/pl/helpers/sort_checks.hpp
+++ lib/include/pl/helpers/sort_checks.hpp
@@ -2,33 +2,27 @@
 
 #include <algorithm>
 
-void sortPredicateError(size_t l_idx, size_t r_idx, const char *pMsg);
-
-template <typename RandomIt>
-void genSortPredicateError(RandomIt collBegin, RandomIt l, RandomIt r, const char *pMsg)
-{
-    sortPredicateError(l-collBegin, r-collBegin, pMsg);
-}
+void sortPredicateError(const char *pMsg);
 
 // Logical implication
 inline bool imp(bool l, bool r) {
     return !(l && !r);
 }
 
-template <typename Predicate, typename RandomIt>
-auto checked_pedicate(RandomIt b, Predicate pred) {
+template <typename Predicate>
+auto checked_pedicate(const Predicate pred) {
     return [=](const auto &l, const auto &r) {
         // Irreflexivity: !(x<x)
         if (pred(l,l))
-            genSortPredicateError(b, l, r, "Irreflexivity: pred(l,l) is false");
+            sortPredicateError("Irreflexivity: pred(l,l) is false");
         if (pred(r,r))
-            genSortPredicateError(b, l, r, "Irreflexivity: pred(r,r) is false");
+            sortPredicateError("Irreflexivity: pred(r,r) is false");
 
         // Asymmetry: if l<r not r<l
         if (!imp(pred(l,r), !pred(r,l)))
-            genSortPredicateError(b, l, r, "Asymmetry: if pred(l,r) then !pred(r,l) is false");
+            sortPredicateError("Asymmetry: if pred(l,r) then !pred(r,l) is false");
         if (!imp(pred(r,l), !pred(l,r)))
-            genSortPredicateError(b, l, r, "Asymmetry: if pred(r,l) then !pred(l,r) is false");
+            sortPredicateError("Asymmetry: if pred(r,l) then !pred(l,r) is false");
 
         return pred(l, r);
     };
@@ -49,7 +43,7 @@ void transitivity(const Iter b, const Iter e, const Predicate pred)
     for (Iter l=b; l<e-2; ++l) {
         for (Iter r=l+2; r<e; ++r) {
             if (!pred(*l, *r)) {
-                genSortPredicateError(b, l, r, "Transitivity");
+                sortPredicateError("Transitivity");
                 // For all x (at index n) in [b, e-1): 
                 //  pred(b[n], b[n+1]) == true
                 // pred(*l, *r) returned false however. This is in violation
@@ -71,7 +65,7 @@ void transitivity_of_incomparability(const Iter b, const Iter e, const Predicate
     for (Iter l=b; l<e-2; ++l) {
         for (Iter r=l+2; r<e; ++r) {
             if (!(!pred(*l, *r) && !pred(*r, *l))) {
-                genSortPredicateError(b, l, r, "Transitivity of incomparability");
+                sortPredicateError("Transitivity of incomparability");
                 // For all x (at index n) in [b, e-1): 
                 //  pred(b[n], b[n+1]) == false && pred(b[n+1], b[n]) == false
                 // (!pred(*l, *r) && !pred(*r, *l)) returned false however. This is in violation
@@ -103,20 +97,20 @@ void post_sort_check(const Iter b, const Iter e, const Predicate pred) {
         else {
             // !pred(*l, *(l+1)) && pred(*(l+1), *l)
             // So l>=r && l>r
-            genSortPredicateError(b, l, l+1, "Not sorted"); // NOT sorted!
+            sortPredicateError("Not sorted"); // NOT sorted!
             ++l;
         }
     }
 }
 
 template<typename RandomIt, typename Compare>
-void checked_sort(RandomIt b, RandomIt e, Compare comp) {
-    std::sort(b, e, checked_pedicate(b, comp));
-    post_sort_check(b, e, comp);
+void checked_sort(RandomIt first, RandomIt last, Compare comp) {
+    std::sort(first, last, checked_pedicate(comp));
+    post_sort_check(first, last, comp);
 }
 
 template<typename RandomIt, typename Compare>
-void checked_stable_sort(RandomIt b, RandomIt e, Compare comp) {
-    std::stable_sort(b, e, checked_pedicate(b, comp));
-    post_sort_check(b, e, comp);
+void checked_stable_sort(RandomIt first, RandomIt last, Compare comp) {
+    std::stable_sort(first, last, checked_pedicate(comp));
+    post_sort_check(first, last, comp);
 }
--- lib/source/pl/helpers/sort_checks.cpp
+++ lib/source/pl/helpers/sort_checks.cpp
@@ -1,7 +1,6 @@
 #include <pl/helpers/sort_checks.hpp>
 #include <iostream>
 
-void sortPredicateError(size_t l_idx, size_t r_idx, const char *pMsg) {
-    (void)l_idx; (void)r_idx;
+void sortPredicateError(const char *pMsg) {
     std::cout << pMsg << std::endl;
 }
