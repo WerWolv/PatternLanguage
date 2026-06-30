073a7c54cadc8cbfd82da7ea809ddb2d72734573
WerWolv <werwolv98@gmail.com>
2026-06-30 22:42:36 +0200

    evaluator: Fix placing imported types in custom sections

--- tests/include/test_patterns/test_pattern_import.hpp
+++ tests/include/test_patterns/test_pattern_import.hpp
@@ -13,11 +13,19 @@ namespace pl::test {
         [[nodiscard]] std::string getSourceCode() const override {
             return R"(
                 import IA as A;
+                import * from ImportedSection as ImportedSection;
+
                 // once include tests
                 import IC as C; // should do nothing, IC as C was transitively imported from A
                 import IC; // should work as expected (import IC without any alias) [ c ]
                 import IC; // should do nothing, IC was already imported
                 import IC as C2; // should work as expected (import IC with alias C2) [ C2::c ]
+
+                auto data = builtin::std::mem::create_section("data");
+                builtin::std::mem::copy_value_to_section("AB", data, 0);
+
+                ImportedSection imported @ 0 in data;
+                std::assert(imported == 'A', "Imported type was not placed in the target section");
 
                 fn main() {
                     A::a();
